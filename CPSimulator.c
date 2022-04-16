26b4970e3f8a423a76a018757967bcbe6e43b9d5
HeshamTB <hishaminv@gmail.com>
2022-04-17 00:04:53 +0300

    main: parase args and correct defualt defs
    	- Parse arguments in main
    	- Correct defualts (defines) to match
    	homework document.
    		IN_VALETS  5 to 3
    		QUEUE_SIZE 5 to 8
    
    Signed-off-by: HeshamTB <hishaminv@gmail.com>

--- CPSimulator.c
+++ CPSimulator.c
@@ -1,13 +1,85 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include "Queue.h"
+#include <stdlib.h>
+#include "CPSimulator.h"
+#include "CarPark.h"
 #include "in-valet.h"
 
 
 int oc, nm, psize;
 long nc, pk, rf, sqw, spt;
 
-int main() {
-    printf("Car Park Simulator\n");
+/**
+    Entry point of program
+    Parse runtime arguments, start GUI and threads
+    @author Muhannad Al-Ghamdi
+    @author Hesham T. Banafa
+    @date Apr 15th, 2022
+*/
+int main(int argc, char *argv[]) 
+{
+    printf("Car Park Simulator\n\
+Muhannad Al-Ghamdi - Hesham T. Banafa\n");
+
+    oc = 0;
+    nm = 0;
+    psize = PARK_SIZE;
+    nc = 0;
+    pk = 0;
+    rf = 0;
+    sqw = 0;
+    spt = 0;
+
+    int in_valets = IN_VALETS;
+    int out_valets = OUT_VALETS;
+    int qsize = QUEUE_SIZE;
+    double exp_cars = EXP_CARS;
+
+    /* Process arguments */
+    /* Redundant code, we can replace with macro or inline function */
+    for (int i = 0; i < argc; i++) {
+        if (i == 1) {
+            int psize_in = atoi(argv[i]);
+            if (psize_in > PARK_LIMIT) psize = PARK_LIMIT;
+            else if (psize_in < 12)    psize = 12; 
+            else                       psize = psize_in;
+
+        } else if (i == 2) {
+            int in_valets_in = atoi(argv[i]);
+            if (in_valets_in > VALET_LIMIT) in_valets = VALET_LIMIT;
+            else if (in_valets_in < 1)      in_valets = 1;
+            else                            in_valets = in_valets_in;
+
+        } else if (i == 3) {
+            int out_valets_in = atoi(argv[i]);
+            if (out_valets_in > VALET_LIMIT) out_valets = VALET_LIMIT;
+            else if (out_valets_in < 1)      out_valets = 1; 
+            else                             out_valets = out_valets_in;
+
+        } else if (i == 4) {
+            int qsize_in = atoi(argv[i]);
+            if (qsize_in > QUEUE_LIMIT) qsize = QUEUE_LIMIT;
+            else if (qsize_in < 3)      qsize = 3;
+            else                        qsize = qsize_in;
+
+        } else if (i == 5) {
+            double exp_car_in = atof(argv[i]);
+            if (exp_car_in > 1.5)       exp_cars = 0.05;
+            else if (exp_car_in < 0.01) exp_cars = 0.01;
+            else                        exp_cars = exp_car_in;
+        }
+    }
+    printf("%d %d %d %d %.2f\n", psize, in_valets, out_valets, qsize, exp_cars);
+    /* if some of the optional args are not set to a non-zero, init with default */
+    return 0;
+}
+
+/**
+    Print help in case of incorrect usage
+    @author Hesham T. Banafa
+    @date Apr 16th, 2022
+*/
+void usage()
+{
+    fprintf(stderr, "usage: carpark [psize inval outval qsize expnum]");
 }
--- CPSimulator.h
+++ CPSimulator.h
@@ -2,3 +2,5 @@
 
 extern int oc, nm, psize;
 extern long nc, pk, rf, sqw, spt;
+
+void usage();
